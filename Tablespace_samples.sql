1cf89288eb5e651e477baeff3276705b7669819d
Banna <87269794+Bhansaa@users.noreply.github.com>
2022-08-30 12:09:04 +0800

    Update Tablespace_samples.sql

--- Tablespace_samples.sql
+++ Tablespace_samples.sql
@@ -21,10 +21,105 @@ select u.tablespace_name tablespace, u.status, sum(u.bytes)/1024/1024 sum_in_mb,
 from dba_undo_extents u  group by u.tablespace_name, u.status order by 1,2
 
 
+
+            ***********************************************
+                check tablespace size
+            ***********************************************
+            
+            
+
+ SELECT tablespace_name
+  , CASE WHEN TRUNC((max_mb - (total_mb - free_mb)) / max_mb * 100, 2) < 10 THEN 'ALERT' END AS alert_flag
+  , TRUNC((max_mb - (total_mb - free_mb)) / max_mb * 100, 2) AS pct_free_mb
+  , TRUNC((max_mb - (total_mb - free_mb)), 2) AS max_free_mb
+  , TRUNC(max_mb, 2) AS max_mb
+  , TRUNC(total_mb, 2) AS total_mb
+  , TRUNC(free_mb, 2) AS free_mb
+FROM
+  ( SELECT tablespace_name
+      , ( SELECT SUM(BYTES) / 1048576 AS free_mb
+          FROM dba_free_space
+          WHERE tablespace_name = dba_data_files.tablespace_name) AS free_mb
+      , SUM(BYTES) / 1048576 AS total_mb
+      , SUM(CASE autoextensible WHEN 'YES' THEN maxbytes ELSE BYTES END) / 1048576 AS max_mb
+    FROM dba_data_files
+    GROUP BY tablespace_name )
+ORDER BY 3
+;
+
+
+
+*******************************************************************************************************************
+  -- 3. Check table fragmentations and generate shrink table statements 
+
+-- Ref: https://orahow.com/how-to-find-and-remove-table-fragmentation-in-oracle-database/ 
+
+-- Recommendation: If you find more than 20% fragmentation then you can proceed for de-fragmentation 
+********************************************************************************************************************
+
+WITH u AS (SELECT 'RFARCH' AS userid FROM DUAL)
+  SELECT table_name,
+         tablespace_name,
+         partitioned,
+         last_analyzed,
+         (SELECT ROUND (SUM (BYTES / 1024 / 1024), 2)     size_mb
+            FROM user_segments
+           WHERE     segment_type IN ('TABLE', 'TABLE PARTITION')
+                 AND segment_name = all_tables.table_name)
+             AS segment_size_mb,
+         ROUND ((blocks * 8) / 1024, 2)
+             size_mb,
+         ROUND ((num_rows * avg_row_len / 1024) / 1024, 2)
+             actual_data_mb,
+         (  ROUND ((blocks * 8) / 1024, 2)
+          - ROUND ((num_rows * avg_row_len / 1024) / 1024, 2))
+             wasted_space_mb,
+         CASE
+             WHEN ROUND ((blocks * 8) / 1024, 2) > 100
+             THEN
+                   ROUND (
+                       (  ROUND (
+                              (  (blocks * 8 / 1024)
+                               - (num_rows * avg_row_len / 1024 / 1024)),
+                              2)
+                        / ROUND (((blocks * 8 / 1024)), 2)),
+                       4)
+                 * 100
+             ELSE
+                 0
+         END
+             AS percentage,
+            'ALTER TABLE '
+         || u.userid
+         || '.'
+         || table_name
+         || ' ENABLE ROW MOVEMENT;'
+         || CHR (13)
+         || CHR (10)
+         || --'ALTER TABLE ' || u.userid || '.' || table_name || ' SHRINK SPACE CASCADE;' || CHR(13) || CHR(10) ||
+
+            'ALTER TABLE '
+         || u.userid
+         || '.'
+         || table_name
+         || ' SHRINK SPACE;'
+         || CHR (13)
+         || CHR (10)
+         || 'ALTER TABLE '
+         || u.userid
+         || '.'
+         || table_name
+         || ' DISABLE ROW MOVEMENT;'
+             AS shrink_table_sql
+    FROM all_tables JOIN u ON u.userid = all_tables.owner
+   WHERE (ROUND ((blocks * 8), 2) > ROUND ((num_rows * avg_row_len / 1024), 2))
+ORDER BY 5 DESC NULLS LAST;
+
+
             ***************************************
                 Query to check long running SQL
            ****************************************
-           
+          
            
 SELECT s.sid,
          s.serial#,
